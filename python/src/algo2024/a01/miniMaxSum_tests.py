33ad54c27757ab2fbef8dd1e3742ad30204ecd3e
iboeobi <iboeobi@gmail.com>
2024-08-28 01:41:41 +0000

    improved coverage updated justfile with lcov data generation command

--- python/src/algo2024/a01/miniMaxSum_tests.py
+++ python/src/algo2024/a01/miniMaxSum_tests.py
@@ -1,9 +1,16 @@
 import pytest
-from .miniMaxSum import miniMaxSum
+from .miniMaxSum import miniMaxSum, miniMaxSum_manual
 
     
 def test_happy_sample(capfd):
     miniMaxSum([1,2,3,4,5])
+    out, err = capfd.readouterr()
+    assert out == "10 14\n"
+    assert err == ''
+
+
+def test_happy_sample_manual(capfd):
+    miniMaxSum_manual([1,2,3,4,5])
     out, err = capfd.readouterr()
     assert out == "10 14\n"
     assert err == ''
